d7da128f997daf4bffd8db52690824d8678e89de
Petros <psdeme@hotmail.com>
2020-01-28 21:06:03 +0200

    fixes: typos, lintering

--- examples/app.js
+++ examples/app.js
@@ -4,30 +4,30 @@ var hostname = '127.0.0.1';
 var port = 3000;
 var ethAirBalloons = require('../lib');
 var server = http.createServer(function (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
 });
 
 server.listen(port, hostname, function () {
-  console.log('Server running at http://' + hostname + ':' + port);
-  var savePath = path.resolve(__dirname + '/contracts');
-  var ethAirBalloonsProvider = ethAirBalloons('http://localhost:8545', savePath);
-   var Car = ethAirBalloonsProvider.createSchema({
-	name: "Car",
-	contractName: "carsContract",
-	properties: [
-	{ name: "engine",
-	  type: "bytes32",
-	  primaryKey: true
-	}
-	]
-});
-   Car.deploy(function(contractInstance, error) {
-   	if(!error) {
-   		console.log('deployed on address: ' + contractInstance.options.address);
-   	} else {
-   		console.log(error);
-   	}
-   });
+	console.log('Server running at http://' + hostname + ':' + port);
+	var savePath = path.resolve(__dirname + '/contracts');
+	var ethAirBalloonsProvider = ethAirBalloons('http://localhost:8545', savePath);
+	var Car = ethAirBalloonsProvider.createSchema({
+		name: "Car",
+		contractName: "carsContract",
+		properties: [
+			{ name: "engine",
+				type: "bytes32",
+				primaryKey: true
+			}
+		]
+	});
+	Car.deploy(function(contractInstance, error) {
+		if (!error) {
+			console.log('deployed on address: ' + contractInstance.options.address);
+		} else {
+			console.log(error);
+		}
+	});
 });
