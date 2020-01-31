624337eebc0433a70581c7731983d59a2b5a5346
Petros <psdeme@hotmail.com>
2020-01-31 20:29:58 +0200

    feature: implemented update

--- examples/app.js
+++ examples/app.js
@@ -33,34 +33,54 @@ server.listen(port, hostname, function () {
 			var newCarObject = {engine: "V8", wheels: 4};
 			var newCarObject2 = {engine: "V9", wheels: 4};
 			CarSchema.save(newCarObject, function (res, err) {
-				if (err){
-					console.log(err);
+				if (!err){
+					console.log("V8 SAVED");
 				}
 				CarSchema.save(newCarObject2, function (res, err) {
 					if (!err) {
-						console.log(res);
+						console.log("V9 SAVED");
 					} else {
 						console.log(err);
 					}
+					console.log("ready to find");
 					CarSchema.find(function (res, err) {
 						if (!err) {
+							console.log("RES WITH 2:");
 							console.log(res);
+						} else {
+							console.log("ERROR");
+							console.log(err);
 						}
-					});
-					CarSchema.findById("V9", function (res, err) {
-						if (!err) {
-							CarSchema.deleteById("V9", function (res, err) {
-								if (!err) {
-									CarSchema.find(function (res, err) {
-										if (!err) {
-											console.log(res);
-										} else {
-											console.log(err);
-										}
-									});
-								}
-							});
-						}
+						CarSchema.findById("V9", function (res, err) {
+							if (!err) {
+								CarSchema.deleteById("V9", function (res, err) {
+									if (!err) {
+										CarSchema.find(function (res, err) {
+											if (!err) {
+												console.log("FIND AFTER DELETE V9");
+												console.log(res);
+											} else {
+												console.log(err);
+											}
+										});
+										CarSchema.updateById('V8',{engine: 'V8', wheels: 5}, function (res, err) {
+											if(!err){
+												console.log('V8 updated to 5 wheels');
+												console.log(res);
+												CarSchema.find(function (res, err) {
+													if (!err) {
+														console.log("FIND AFTER V8 UPDATED");
+														console.log(res);
+													} else {
+														console.log(err);
+													}
+												});
+											}
+										})
+									}
+								});
+							}
+						});
 					});
 				});
 			});
